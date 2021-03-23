fe869ef1107e74432287ff253b0debc5022f22a8
Michael Ferreira <ferreira.mjm@gmail.com>
2021-03-23 16:06:06 +0000

    JS created board

---
+++ dist/app.dev.js
@@ -0,0 +1,138 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+  // cards
+  var cardArray = [{
+    name: "baggy",
+    img: "images\Baggy1.png"
+  }, {
+    name: "baggy",
+    img: "images\Baggy1.png"
+  }, {
+    name: "bertie",
+    img: "images\Bertie1.png"
+  }, {
+    name: "bertie",
+    img: "images\Bertie1.png"
+  }, {
+    name: "billy",
+    img: "images\Billy1.png"
+  }, {
+    name: "billy",
+    img: "images\Billy1.png"
+  }, {
+    name: "capt",
+    img: "images\Capt Blade1.png"
+  }, {
+    name: "capt",
+    img: "images\Capt Blade1.png"
+  }, {
+    name: "card-back",
+    img: "images/card-back.png"
+  }, {
+    name: "card-back",
+    img: "images/card-back.png"
+  }, {
+    name: "chirpy",
+    img: "images\Chirpy1.png"
+  }, {
+    name: "chirpy",
+    img: "images\Chirpy1.png"
+  }, {
+    name: "filbert",
+    img: "images\Filbert1.png"
+  }, {
+    name: "filbert",
+    img: "images\Filbert1.png"
+  }, {
+    name: "fred",
+    img: "images\Fred1.png"
+  }, {
+    name: "fred",
+    img: "images\Fred1.png"
+  }, {
+    name: "gully",
+    img: "images\Gully1.png"
+  }, {
+    name: "gully",
+    img: "images\Gully1.png"
+  }, {
+    name: "gunnersaurus",
+    img: "images\Gunnersaurus1.png"
+  }, {
+    name: "gunnersaurus",
+    img: "images\Gunnersaurus1.png"
+  }, {
+    name: "hammerhead",
+    img: "images\Hammerhead1.png"
+  }, {
+    name: "hammerhead",
+    img: "images\Hammerhead1.png"
+  }, {
+    name: "hercules",
+    img: "images\Hercules1.png"
+  }, {
+    name: "hercules",
+    img: "images\Hercules1.png"
+  }, {
+    name: "red",
+    img: "images\Mighty Red1.png"
+  }, {
+    name: "red",
+    img: "images\Mighty Red1.png"
+  }, {
+    name: "monty",
+    img: "images\Monty Magpie1.png"
+  }, {
+    name: "monty",
+    img: "images\Monty Magpie1.png"
+  }, {
+    name: "moonchester",
+    img: "images\Moonchester1.png"
+  }, {
+    name: "moonchester",
+    img: "images\Moonchester1.png"
+  }, {
+    name: "pete",
+    img: "images\Pete1.png"
+  }, {
+    name: "pete",
+    img: "images\Pete1.png"
+  }, {
+    name: "sammy",
+    img: "images\Sammy Saint1.png"
+  }, {
+    name: "sammy",
+    img: "images\Sammy Saint1.png"
+  }, {
+    name: "stamford",
+    img: "images\Stamford1.png"
+  }, {
+    name: "stamford",
+    img: "images\Stamford1.png"
+  }, {
+    name: "white",
+    img: "images\white.png"
+  }, {
+    name: "white",
+    img: "images\white.png"
+  }, {
+    name: "wolfie",
+    img: "images\Wolfie & Wendy1.png"
+  }, {
+    name: "wolfie",
+    img: "images\Wolfie & Wendy1.png"
+  }]; // Create gameboard grid
+
+  var grid = document.querySelector('.grid');
+
+  function createBoard() {
+    for (var i = 0; i < cardArray.length; i++) {
+      var card = document.createElement('img');
+      card.setAttribute('src', 'images/card-back.png');
+      card.setAttribute('data-id', i); //card.addEventListener('click', flipcard)//
+
+      grid.appendChild(card);
+    }
+  }
+});
