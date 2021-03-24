84c02c409854bffe09b6bd530298a1bd963164b5
Michael Ferreira <ferreira.mjm@gmail.com>
2021-03-24 15:45:42 +0000

    Create board

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
- // cards
+ // declare cards
  const cardArray = [
    {
     name: "baggy",
@@ -21,17 +21,11 @@ document.addEventListener('DOMContentLoaded', () => {
     name: "billy",
     img: "images\Billy1.png"},
    {
-    name: "capt",
-    img: "images\Capt Blade1.png"},
-   {
-    name: "capt",
-    img: "images\Capt Blade1.png"},
-   {
-    name: "card-back",
-    img: "images/card-back.png"},
-   {
-    name: "card-back",
-    img: "images/card-back.png"},
+    name: "gunner",
+    img: "images\Gunnersaurus1.png"},
+    {
+    name: "gunner",
+    img: "images\Gunnersaurus1.png"},
    {
     name: "chirpy",
     img: "images\Chirpy1.png"},
@@ -43,98 +37,29 @@ document.addEventListener('DOMContentLoaded', () => {
     img: "images\Filbert1.png"},
    {
     name: "filbert",
-    img: "images\Filbert1.png"},
-   {
-    name: "fred",
-    img: "images\Fred1.png"},
-   {
-    name: "fred",
-    img: "images\Fred1.png"},
-   {
-    name: "gully",
-    img: "images\Gully1.png"},
-   { 
-    name: "gully",
-    img: "images\Gully1.png"},
-   {
-    name: "gunnersaurus",
-    img: "images\Gunnersaurus1.png"},
-   {
-    name: "gunnersaurus",
-    img: "images\Gunnersaurus1.png"},
-   {
-    name: "hammerhead",
-    img: "images\Hammerhead1.png"},
-   {
-    name: "hammerhead",
-    img: "images\Hammerhead1.png"},
-   {
-    name: "hercules",
-    img: "images\Hercules1.png"},
-   {
-    name: "hercules",
-    img: "images\Hercules1.png"},
-   {
-    name: "red",
-    img: "images\Mighty Red1.png"},
-   {
-    name: "red",
-    img: "images\Mighty Red1.png"},
-   {
-    name: "monty",
-    img: "images\Monty Magpie1.png"},
-   {
-    name: "monty",
-    img: "images\Monty Magpie1.png"},
-   {
-    name: "moonchester",
-    img: "images\Moonchester1.png"},
-   {
-    name: "moonchester",
-    img: "images\Moonchester1.png"},
-   {
-    name: "pete",
-    img: "images\Pete1.png"},
-   {
-    name: "pete",
-    img: "images\Pete1.png"},
-   {
-    name: "sammy",
-    img: "images\Sammy Saint1.png"},
-   {
-    name: "sammy",
-    img: "images\Sammy Saint1.png"},
-   {
-    name: "stamford",
-    img: "images\Stamford1.png"},
-   {
-    name: "stamford",
-    img: "images\Stamford1.png"},
-   {
-    name: "white",
-    img: "images\white.png"},
-   {
-   name: "white",
-   img: "images\white.png"},
-   {
-    name: "wolfie",
-    img: "images\Wolfie & Wendy1.png"},
-   {
-    name: "wolfie",
-    img: "images\Wolfie & Wendy1.png"},
+    img: "images\Filbert1.png"}
   ]
-  // Create gameboard grid
+
+  //add cardArray.sort ->
+
   const grid = document.querySelector('.grid')
 
+  // Create gameboard grid
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
-      var card = document.createElement('img')
-      card.setAttribute('src', 'images/card-back.png')
-      card.setAttribute('data-id', i)
-      //card.addEventListener('click', flipcard)//
-      grid.appendChild(card)
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images\card-back.png')
+        card.setAttribute('data-id', i)
+        grid.appendChild(card)
+        //need to addEventListener('click',//
     }
   }
 
+  // add matching card functions//
 
+  // add card flip function//
+
+
+
+  createBoard()
 })
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,7 +1,7 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function () {
-  // cards
+  // declare cards
   var cardArray = [{
     name: "baggy",
     img: "images\Baggy1.png"
@@ -21,17 +21,11 @@ document.addEventListener('DOMContentLoaded', function () {
     name: "billy",
     img: "images\Billy1.png"
   }, {
-    name: "capt",
-    img: "images\Capt Blade1.png"
+    name: "gunner",
+    img: "images\Gunnersaurus1.png"
   }, {
-    name: "capt",
-    img: "images\Capt Blade1.png"
-  }, {
-    name: "card-back",
-    img: "images/card-back.png"
-  }, {
-    name: "card-back",
-    img: "images/card-back.png"
+    name: "gunner",
+    img: "images\Gunnersaurus1.png"
   }, {
     name: "chirpy",
     img: "images\Chirpy1.png"
@@ -44,95 +38,20 @@ document.addEventListener('DOMContentLoaded', function () {
   }, {
     name: "filbert",
     img: "images\Filbert1.png"
-  }, {
-    name: "fred",
-    img: "images\Fred1.png"
-  }, {
-    name: "fred",
-    img: "images\Fred1.png"
-  }, {
-    name: "gully",
-    img: "images\Gully1.png"
-  }, {
-    name: "gully",
-    img: "images\Gully1.png"
-  }, {
-    name: "gunnersaurus",
-    img: "images\Gunnersaurus1.png"
-  }, {
-    name: "gunnersaurus",
-    img: "images\Gunnersaurus1.png"
-  }, {
-    name: "hammerhead",
-    img: "images\Hammerhead1.png"
-  }, {
-    name: "hammerhead",
-    img: "images\Hammerhead1.png"
-  }, {
-    name: "hercules",
-    img: "images\Hercules1.png"
-  }, {
-    name: "hercules",
-    img: "images\Hercules1.png"
-  }, {
-    name: "red",
-    img: "images\Mighty Red1.png"
-  }, {
-    name: "red",
-    img: "images\Mighty Red1.png"
-  }, {
-    name: "monty",
-    img: "images\Monty Magpie1.png"
-  }, {
-    name: "monty",
-    img: "images\Monty Magpie1.png"
-  }, {
-    name: "moonchester",
-    img: "images\Moonchester1.png"
-  }, {
-    name: "moonchester",
-    img: "images\Moonchester1.png"
-  }, {
-    name: "pete",
-    img: "images\Pete1.png"
-  }, {
-    name: "pete",
-    img: "images\Pete1.png"
-  }, {
-    name: "sammy",
-    img: "images\Sammy Saint1.png"
-  }, {
-    name: "sammy",
-    img: "images\Sammy Saint1.png"
-  }, {
-    name: "stamford",
-    img: "images\Stamford1.png"
-  }, {
-    name: "stamford",
-    img: "images\Stamford1.png"
-  }, {
-    name: "white",
-    img: "images\white.png"
-  }, {
-    name: "white",
-    img: "images\white.png"
-  }, {
-    name: "wolfie",
-    img: "images\Wolfie & Wendy1.png"
-  }, {
-    name: "wolfie",
-    img: "images\Wolfie & Wendy1.png"
-  }]; // Create gameboard grid
+  }]; //add cardArray.sort ->
 
-  var grid = document.querySelector('.grid');
+  var grid = document.querySelector('.grid'); // Create gameboard grid
 
   function createBoard() {
     for (var i = 0; i < cardArray.length; i++) {
       var card = document.createElement('img');
-      card.setAttribute('src', 'images/card-back.png');
-      card.setAttribute('data-id', i); //card.addEventListener('click', flipcard)//
-
-      grid.appendChild(card);
+      card.setAttribute('src', 'images\card-back.png');
+      card.setAttribute('data-id', i);
+      grid.appendChild(card); //need to addEventListener('click',//
     }
-  }
+  } // add matching card functions//
+  // add card flip function//
+
+
+  createBoard();
 });
