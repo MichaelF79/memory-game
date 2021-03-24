d13883428f8b27381600dfb7638bdaba0d0c3bcd
Michael Ferreira <ferreira.mjm@gmail.com>
2021-03-24 18:00:43 +0000

    Gameboard set out, some JS added

--- app.js
+++ app.js
@@ -4,51 +4,92 @@ document.addEventListener('DOMContentLoaded', () => {
  const cardArray = [
    {
     name: "baggy",
-    img: "images\Baggy1.png"},
+    img: "images/Baggy1.png"},
    {
     name: "baggy",
-    img: "images\Baggy1.png"},
+    img: "images/Baggy1.png"},
    {
     name: "bertie",
-    img: "images\Bertie1.png"},
+    img: "images/Bertie1.png"},
    {
     name: "bertie",
-    img: "images\Bertie1.png"},
+    img: "images/Bertie1.png"},
    {
     name: "billy",
-    img: "images\Billy1.png"},
+    img: "images/Billy1.png"},
    {
     name: "billy",
-    img: "images\Billy1.png"},
+    img: "images/Billy1.png"},
    {
-    name: "gunner",
-    img: "images\Gunnersaurus1.png"},
+    name: "capt",
+    img: "images/CaptBlade1.png"},
     {
+    name: "capt",
+    img: "images/CaptBlade1.png"},
+   {
+    name: "chirpy",
+    img: "images/Chirpy1.png"},
+   {
+    name: "chirpy",
+    img: "images/Chirpy1.png"},
+   {
+    name: "filbert",
+    img: "images/Filbert1.png"},
+   {
+    name: "filbert",
+    img: "images/Filbert1.png"},
+   {
+    name: "fred",
+    img: "images/Fred1.png"},
+   {
+    name: "fred",
+    img: "images/Fred1.png"},
+   {
+    name: "gully",
+    img: "images/Gully1.png"},
+   {
+    name: "gully",
+    img: "images/Gully1.png"},
+   {
     name: "gunner",
-    img: "images\Gunnersaurus1.png"},
+    img: "images/Gunnersaurus1.png"}, 
    {
-    name: "chirpy",
-    img: "images\Chirpy1.png"},
+    name: "gunner",
+    img: "images/Gunnersaurus1.png"},
    {
-    name: "chirpy",
-    img: "images\Chirpy1.png"},
+    name: "hammer",
+    img: "images/Hammerhead1.png"},
    {
-    name: "filbert",
-    img: "images\Filbert1.png"},
+    name: "hammer",
+    img: "images/Hammerhead1.png"},
    {
-    name: "filbert",
-    img: "images\Filbert1.png"}
+    name: "hercules",
+    img: "images/Hercules1.png"},
+   {
+    name: "hercules",
+    img: "images/Hercules1.png"},
+   {
+    name: "red",
+    img: "images/MightyRed1.png"},
+   {
+    name: "red",
+    img: "images/MightyRed1.png"},
+   
   ]
 
   //add cardArray.sort ->
 
   const grid = document.querySelector('.grid')
+  const resultDisplay = document.querySelector('#result')
+  let cardsChosen = []
+  let cardsChosenId = []
+  let cardsWon = [];
 
   // Create gameboard grid
-  function createBoard() {
+  function createBoard(){
     for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
-        card.setAttribute('src', 'images\card-back.png')
+        card.setAttribute('src', 'images/card-back.png')
         card.setAttribute('data-id', i)
         grid.appendChild(card)
         //need to addEventListener('click',//
@@ -58,7 +99,15 @@ document.addEventListener('DOMContentLoaded', () => {
   // add matching card functions//
 
   // add card flip function//
-
+  function flipCard() {
+    let cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500)
+    }
+  }
 
 
   createBoard()
